6e38357058d61afecc077711ec9a0690c5c21f35
Brian Talbert <btalb2653@gmail.com>
2022-05-30 05:58:38 -0400

    Problem 22

--- Problem22.py
+++ Problem22.py
@@ -1,11 +1,25 @@
 import sys
 import csv
 
+def calc(names):
+	s = 0
+	i = 1
+	for name in names:
+		char_sum = 0
+		for c in name:
+			char_sum += ord(c) - 64
+		char_sum *= i
+		s += char_sum
+		i += 1
+	return s
+
+
 def main(f):
 	f_reader = csv.reader(f, delimiter=',')
 	names = list(f_reader)[0]
 	names.sort()		
-	print(names)
+	s = calc(names)
+	print(s)
 
 
 if __name__ == "__main__":
